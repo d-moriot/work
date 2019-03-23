26e49b56797ce8bee486bea5fd5603f3e6b70736
d-moriot <moriot.md5.daiki@gmail.com>
2019-03-23 17:59:49 +0900

    thr

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ function zipcloud(){
           type: "GET",
           url: "http://zipcloud.ibsnet.co.jp/api/search",
           async: true, //trueなら非同期処理。falseなら同期処理。
+          data:{
+              zipcode: 1300002
+          },
           succses: function(mes){
               console.log(mes)
           }
