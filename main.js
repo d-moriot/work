62def48a91512b0b8cc001e0baec925ebb145561
d-moriot <moriot.md5.daiki@gmail.com>
2019-03-23 22:59:14 +0900

    4

--- main.js
+++ main.js
@@ -1,20 +1,11 @@
 function testapi(){
-    $.getJSON("test.json",function(data) {
-        console.log(data);
-        $('#area').val(data.name)
-    });
-}
+    var request = new XMLHttpRequest();
+    request.open('GET', '/test.json');
+    request.responseType = 'json';
+    request.send();
 
-function zipcloud(){
-        $.ajax({
-          type: "GET",
-          url: "https://api.github.com/search/repositories",
-          async: true, //trueなら非同期処理。falseなら同期処理。
-          data:{
-              q: "javascript"
-          },
-          succses: function(mes){
-              console.log(mes);
-          }
-        });
+    request.onload = function() {
+        var introduction = request.response;
+        console.log(introduction);
+      };
 }
