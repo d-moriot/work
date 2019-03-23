4425f49d03bc661c6f0d11675dcfbe54b559c62f
d-moriot <moriot.md5.daiki@gmail.com>
2019-03-23 18:43:22 +0900

    4

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
 function testapi(){
-    console.log('push')
+    $.getJSON("test.json",function(data) {
+        console.log(data);
+        document.getElementById();
+    });
+
+    $.ajax({
+        type: json,
+        url: test,
+        async: true, //trueなら非同期処理。falseなら同期処理。
+        succses: function(mes){
+            console.log(mes)
+            $('#area').val(mes)
+        }
+      });
 }
 
 function zipcloud(){
         $.ajax({
           type: "GET",
-          url: "//zipcloud.ibsnet.co.jp/api/search",
+          url: "http//zipcloud.ibsnet.co.jp/api/search",
           async: true, //trueなら非同期処理。falseなら同期処理。
           data:{
               zipcode: 1300002
