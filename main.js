2ba3d35bf89378f2bcfb4013bcd1c5221e884499
d-moriot <moriot.md5.daiki@gmail.com>
2019-03-23 21:31:11 +0900

    4

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ function testapi(){
 function zipcloud(){
         $.ajax({
           type: "GET",
-          url: "http//zipcloud.ibsnet.co.jp/api/search/",
+          url: "http//zipcloud.ibsnet.co.jp/api/search",
           async: true, //trueなら非同期処理。falseなら同期処理。
           data:{
-              zipcode: 130-0002
+              zipcode: 1300002
           },
           succses: function(mes){
               console.log(mes)
